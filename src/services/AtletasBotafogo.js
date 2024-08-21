3986540efcc4c0986c8ee84cbbef0adbb999abb5
leonardovaz5 <161666568+leonardovaz5@users.noreply.github.com>
2024-08-21 19:30:12 -0300

    Update AtletasBotafogo.js

--- src/services/AtletasBotafogo.js
+++ src/services/AtletasBotafogo.js
@@ -1,24 +1,23 @@
 import { handleError, handleResponse } from "./ServiceUtils";
 
-const url = "https://botafogo-atletas.mange.li"
+const url = "https://botafogo-atletas.mange.li";
+
 
 const getAll = async () => {
-  const response = await (fetch(`${url}/all`).catch(handleError));
-
+  const response = await (fetch(`${url}/all?ano=2024`).catch(handleError));
   return await handleResponse(response);  
 }
 
-const getElenco = async (elenco = 'masculino') => {
-  const response = await (fetch(`${url}/${elenco}`).catch(handleError));
 
+const getElenco = async (elenco = 'masculino') => {
+  const response = await (fetch(`${url}/${elenco}?ano=2024`).catch(handleError));
   return await handleResponse(response);
 }
+
 
 const getAtleta = async (atleta) => {
   const response = await (fetch(`${url}/${atleta}`).catch(handleError));
-  
   return await handleResponse(response);
 }
-
 
 export { getAll, getElenco, getAtleta };
